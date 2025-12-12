2d5afe69709c0e8cc8d02d3950141d59b5f7f828
pante31 <alex.jump01@gmail.com>
2025-12-12 18:59:21 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -219,12 +219,12 @@ else:
             timestamp,
             user['session_id'],
             user.get('language', 'it'), # Colonna lingua
-            user['age'],
             user['gender'],
+            user['age'],
             user['education'],
             texto['id'],
             texto['text'],
-            m1, m2, m3
+            m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11
         ]
         
         successo = False

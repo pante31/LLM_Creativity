be2ed036cc9b9a9d99c8fa43297fe26e60147b7b
pante31 <alex.jump01@gmail.com>
2025-12-15 17:02:24 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -291,6 +291,7 @@ else:
             user['education'],
             user['experience'],
             texto['id'],
+            texto['author'],
             texto['text'],
             m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11
         ]
